5cf6aa43bc5ee393ead71575f3225806a46fd8a8
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2022-09-27 03:24:35 +0530

    text and container

--- flutter/app/lib/main.dart
+++ flutter/app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +14,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("flutter app")),
-      body: Container(
-        child: Center(child: Text("Hi flutter")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.red, Colors.green]),
+              // shape: BoxShape.circle // koi bhi shape dene ke liye
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 5,
+                    offset: Offset(2.0, 5.0))
+              ]),
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.yellow,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
       ),
     );
   }
