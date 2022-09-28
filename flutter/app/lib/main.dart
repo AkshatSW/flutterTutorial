1d871f68f168101a483df2439c06d87040c803f1
Akshat Srivastava <akshatsrivastava200213@gmail.com>
2022-09-28 11:35:01 +0530

    stateful widgets, text change kaise hoga apne aap ye sab set kiya hai ham logo ne

--- flutter/app/lib/main.dart
+++ flutter/app/lib/main.dart
@@ -1,57 +1,127 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_field, prefer_final_fields
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "First flutter app",
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "Flutter 3",
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.red),
+    ),
+  );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  TextEditingController _nameController =
+      TextEditingController(); //_nameController me _ use karne se ye ek private value ho jayegi
+  var myText = "Change Me";
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("flutter app")),
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(title: Text("Flutter App")),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-            child: Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            width: 200,
-            height: 400,
-            color: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // horizontal
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          //this will make sure ki jab hamara keyboard aaye to ye usko overflow na kare
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/bg.jpeg",
+                  fit: BoxFit
+                      .cover, //basically kaise fit hogi image uske liye  hai ye
+                  // width: 100,
+                  // height: 100,
+                ),
+                SizedBox(
+                  height: 20,
+                ), //this is basically like an empty div
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType
+                        .text, //isko agar number kar denge to shirf number wala keyboard dikhayega
+                    // obscureText: true,
+                    //ye password fields ke liye acha option hai
+                    decoration: InputDecoration(
+                      hintText: " Enter Something Here",
+                      labelText: "Name",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
-        )),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            // DrawerHeader(
+            //   // ignore: sort_child_properties_last
+            //   child: Text(
+            //     "Hi, Akshat",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.indigo),
+            // ),
+
+            UserAccountsDrawerHeader(
+              accountName: Text("Akshat Srivastava"),
+              accountEmail: Text("akshat@zariyaa.in"),
+              currentAccountPicture: CircleAvatar(
+                // child: Image.network(
+                //     "https://instagram.flko7-1.fna.fbcdn.net/v/t51.2885-19/281551560_166930375728389_901818772048826142_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.flko7-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=tkSk93nfHJMAX_uBaRy&edm=ABmJApABAAAA&ccb=7-5&oh=00_AT-0E6mFl40ul8okUUjHU5YgwJkvkxgX7l8P4MNVdRQqxg&oe=633A2602&_nc_sid=6136e7"), //circle nahi hoga try the below method
+                backgroundImage: NetworkImage(
+                    "https://instagram.flko7-1.fna.fbcdn.net/v/t51.2885-19/281551560_166930375728389_901818772048826142_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.flko7-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=tkSk93nfHJMAX_uBaRy&edm=ABmJApABAAAA&ccb=7-5&oh=00_AT-0E6mFl40ul8okUUjHU5YgwJkvkxgX7l8P4MNVdRQqxg&oe=633A2602&_nc_sid=6136e7"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text('Personal'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text('Email'),
+              trailing: Icon(Icons.send),
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+          //ye method call karna padega to make sure ki change ho raha hai
+        },
+        child: Icon(Icons.refresh),
       ),
     );
   }
